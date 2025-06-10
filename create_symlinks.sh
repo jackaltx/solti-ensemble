c5fdc6f3a8186958e9de77755a8d412ae32598f0
jackal <jack@lavnet.net>
2025-06-09 19:04:58 -0500

    update documentation, prune

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# This allows you to download the git repository and "fake" installing the collection.
+# You cannot use this for testing, but it is handy for development as 
+# changed are pushed out real-time.
+
+# This assumes you did not change the location in your running ansible.cfg
 # Create the ansible collections directory structure if it doesn't exist
 mkdir -p ~/.ansible/collections/ansible_collections/jackaltx
 
